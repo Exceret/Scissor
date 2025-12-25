2081f9f0f169e3b032ec0734bb0f79c5a0cbb6fc
Exceret <15364051195@163.com>
2025-12-25 09:52:55 +0800

    A substitution for base correlation

--- R/Scissor.R
+++ R/Scissor.R
@@ -154,7 +154,11 @@ Scissor.v5.optimized <- function(
             )
         }
 
-        X <- stats::cor(Expression_bulk, Expression_cell)
+        X <- if (rlang::is_installed("WGCNA")) {
+            WGCNA::cor(Expression_bulk, Expression_cell)
+        } else {
+            stats::cor(Expression_bulk, Expression_cell)
+        }
 
         quality_check <- SigBridgeRUtils::colQuantiles3(
             X,
@@ -292,8 +296,6 @@ Scissor.v5.optimized <- function(
     alpha <- alpha %||%
         c(0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
 
-    results <- list()
-
     for (i in seq_along(alpha)) {
         set.seed(seed)
 
@@ -345,7 +347,9 @@ Scissor.v5.optimized <- function(
         }
 
         if (percentage < cutoff) {
-            ts_cli$cli_alert_info(cli::col_green("Scissor Ended."))
+            if (verbose) {
+                ts_cli$cli_alert_info(cli::col_green("Scissor Ended."))
+            }
             break
         }
     }
